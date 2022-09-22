2121438794a7118f78ff8eae624cce3380fa48ad
terraform <terraform@isa-server>
2022-09-22 17:38:01 +0200

    add use of a plugin specific version

--- local_file_with_specific_version/main.tf
+++ local_file_with_specific_version/main.tf
@@ -1,4 +1,12 @@
-# Simple local_file resource
+# Simple local_file resource tha use a specific version of provider
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.1.0"
+    }
+  }
+}
 
 resource "local_file" "pet" {
   filename        = "${path.module}/pets.txt"
